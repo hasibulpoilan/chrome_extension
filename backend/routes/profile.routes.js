d0eeefe3fdc85c895374b0b5a3cbfcae5c556570
hasibulpoilan <hasibulpailan8@gmail.com>
2024-09-24 20:24:01 +0530

    Update profile.controller, User model, and profile routes

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-  const profile = require("../controllers/profile.controller.js");
+  const profile = require("../../controllers/profile.controller.js");
 
 
   app.post("/api/profiles", profile.create);
